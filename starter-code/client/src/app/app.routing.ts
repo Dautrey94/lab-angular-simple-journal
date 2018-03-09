b240826c06b78393cc4f2b745b5d7484fe7bd183
Ana Raballo <anitaraballo@gmail.com>
2018-03-09 10:24:13 -0500

    Inital commit

---
+++ starter-code/client/src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+import { EntryListComponent } from './entry-list/entry-list.component'
+
+export const routes: Routes = [
+    { path: '', component: EntryListComponent },
+    // { path: 'journal/:id', component: EntryDetailsComponent },
+    { path: '**', redirectTo: '' }
+];
